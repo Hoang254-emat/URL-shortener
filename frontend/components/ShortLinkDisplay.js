474ff3b0648ad8bad7df1e4f46373164984ee42c
Hoang254-emat <c2lieuxadobahoang@gmail.com>
2025-03-14 17:32:43 +0700

    Fix frontend folder issue

---
+++ frontend/components/ShortLinkDisplay.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const ShortLinkDisplay = ({ shortUrl, shortCode, qrCode }) => {
+    const [clicks, setClicks] = useState(null);
+
+    useEffect(() => {
+        if (!shortCode) return;
+
+        fetch(`http://localhost:4000/stats/${shortCode}`)
+        .then((res) => res.json())
+        .then((data) => setClicks(data.clicks))
+        .catch((err) => console.error("Error fetching clicks:", err));
+    }, [shortCode]);
+
+    return (
+        <div>
+            <h3>Short Link:</h3>
+            <a href={shortUrl} target="_blank">{shortUrl}</a>
+
+            <h3>QR Code:</h3>
+            {qrCode ? (
+                <img src={qrCode} alt="QR Code" />
+            ) : (
+                <p>QR Code not available</p>
+            )}
+
+
+            <h3>Clicks: {clicks !== null ? clicks : "Loading..."}</h3>
+        </div>
+    );
+};
+
+export default ShortLinkDisplay;
---
+++ frontend/pages/index.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import QRCode from "qrcode.react";
+import ShortLinkDisplay from "@/components/ShortLinkDisplay";
+
+export default function Home() {
+  const [originalUrl, setOriginalUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+  const [qrCode, setQrCode] = useState("");
+  const [clicks, setClicks] = useState(null);
+  const [shortCode, setShortCode] = useState("");
+
+  const handleShorten = async () => {
+    try {
+      const res = await axios.post("http://localhost:4000/shorten", {
+        original_url: originalUrl,
+      });
+      setShortUrl(`http://localhost:4000/${res.data.short_url}`);
+      setQrCode(res.data.qr);
+      setShortCode(res.data.short_url);
+    } catch (error) {
+      console.error("Error shortening URL:", error);
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>URL Shortener</h1>
+
+      <input
+        type="text"
+        placeholder="Enter your URL"
+        value={originalUrl}
+        onChange={(e) => setOriginalUrl(e.target.value)}
+        style={{ padding: "8px", width: "300px", marginBottom: "10px" }}
+      />
+      <br />
+      <button onClick={handleShorten} style={{ padding: "8px 15px" }}>
+        Shorten URL
+      </button>
+
+      {shortCode && (
+        <ShortLinkDisplay shortUrl={shortUrl} shortCode={shortCode} qrCode={qrCode} />
+      )}
+    </div>
+  );
+}
